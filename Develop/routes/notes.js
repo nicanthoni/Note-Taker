9cc97678b356ef27e1de5f27876c4ff69aeee26a
nic-d <nicanthonidiaz@gmail.com>
2023-11-03 15:12:20 -0400

    File strcuture made, express initiated and routes created

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -2,6 +2,17 @@ const notes = require('express').Router();
 // helpers need be created and imported? 
 // FOR READ AND APPEND ^
 
+// helper needed for readfromfile to work?
+// GET route to read the db.json file and return all saved notes as JSON
 notes.get('/', (req, res) => {
-    // readFromFile 
-})
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+});
+
+
+// POST route to receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client
+// Need to find a way to give each note a unique id when it's saved (hint to ook into npm packages)
+notes.post('/', (req, res) => {
+// code here
+});
+
+module.exports = notes;
